5d55def59c9792a955186f453862ccd82765c51b
RealRemeku <jahamuhapaxtf@gmail.com>
2019-08-31 13:57:40 +0300

    Field test 2 passing

--- src/tictactoe.ts
+++ src/tictactoe.ts
@@ -1,9 +1,19 @@
 export class TicTacToe{
+    static activePlayer:string="X"
+    field:string[][]=[[]]
     genField(){
+        this.field = [
+            ["","",""],
+            ["","",""],
+            ["","",""]
+        ]
         return [
             ["","",""],
             ["","",""],
             ["","",""]
         ]
+    }
+    clickHandler(x:number, y:number){
+        
     }
 }
